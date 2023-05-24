e1573de50c3a6600155f8006603a303ae2a6b394
Fuselet <isak.kallini@live.com>
2023-05-24 22:35:25 +0200

    Implemented connection pool to fix problem with connection timing out.

---
+++ src/ConnectionPooling/BasicConnectionPool.java
@@ -0,0 +1,83 @@
+package ConnectionPooling;
+
+import com.fasterxml.jackson.databind.introspect.TypeResolutionContext;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BasicConnectionPool implements ConnectionPool {
+    private String url;
+    private String user;
+    private String password;
+    private List<Connection> connectionPool;
+    private List<Connection> usedConnections = new ArrayList<>();
+    private static int INITIAL_POOL_SIZE = 5;
+    private int MAX_TIMEOUT = 2;
+
+
+    public static BasicConnectionPool create(
+            String url, String user,
+            String password) throws SQLException {
+
+        List<Connection> pool = new ArrayList<>(INITIAL_POOL_SIZE);
+        for (int i = 0; i < INITIAL_POOL_SIZE; i++) {
+            pool.add(createConnection(url, user, password));
+        }
+        return new BasicConnectionPool(url, user, password, pool);
+    }
+
+    public BasicConnectionPool(String url, String user, String password, List<Connection> pool){
+        this.url = url;
+        this.user = user;
+        this.password = password;
+        this.connectionPool = pool;
+    }
+
+    // standard constructors
+
+    @Override
+    public Connection getConnection() throws SQLException {
+        Connection connection = connectionPool
+                .remove(connectionPool.size() - 1);
+        if(!connection.isValid(MAX_TIMEOUT)){
+            connection = createConnection(url, user, password);
+        }
+
+        usedConnections.add(connection);
+        return connection;
+    }
+
+    @Override
+    public boolean releaseConnection(Connection connection) {
+        connectionPool.add(connection);
+        return usedConnections.remove(connection);
+    }
+
+    @Override
+    public String getUrl() {
+        return url;
+    }
+
+    @Override
+    public String getUser() {
+        return user;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    private static Connection createConnection(
+            String url, String user, String password)
+            throws SQLException {
+        return DriverManager.getConnection(url, user, password);
+    }
+
+    public int getSize() {
+        return connectionPool.size() + usedConnections.size();
+    }
+}
